5d5dc1cb4dccda012585f7609e6b180aac5d6038
bshmanair <bhishu888@gmail.com>
2025-12-06 04:23:53 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,25 @@
 #include <sstream>
 #include <vector>
 #include <unordered_set>
+#include <filesystem>
 
-std::vector<std::string> split(const std::string &str)
+std::unordered_set<std::string> builtin = {"exit", "echo", "type"};
+
+std::vector<std::string> split(const std::string &str, char delimiter)
 {
 	std::stringstream ss(str);
 	std::string token;
 	std::vector<std::string> tokens;
-	while (std::getline(ss, token, ' '))
+	while (std::getline(ss, token, delimiter))
 		tokens.push_back(token);
 	return tokens;
+}
+
+bool is_executable(const std::filesystem::path &p)
+{
+	using namespace std::filesystem;
+	auto pr = status(p).permissions();
+	return (pr & (perms::owner_exec | perms::group_exec | perms::others_exec)) != perms::none;
 }
 
 int main()
@@ -20,6 +30,12 @@ int main()
 	std::cout << std::unitbuf;
 	std::cerr << std::unitbuf;
 
+#if _WIN32
+	char separator = ';';
+#else
+	char separator = ':';
+#endif
+
 	std::string input;
 
 	// TODO: Uncomment the code below to pass the first stage
@@ -27,25 +43,41 @@ int main()
 	{
 		std::cout << "$ ";
 		std::getline(std::cin, input);
-		std::vector<std::string> tokens = split(input);
-		std::string command = tokens[0];
-
-		std::unordered_set<std::string> builtin = {"exit", "echo", "type"};
+		std::vector<std::string> tokens = split(input, ' ');
+		std::string command = tokens.at(0);
 
 		if (command == "exit")
 			return 0;
 		else if (command == "echo")
 		{
 			for (int i = 1; i < tokens.size(); i++)
-				std::cout << tokens[i] << " ";
+				std::cout << tokens.at(i) << " ";
 			std::cout << std::endl;
 		}
 		else if (command == "type")
 		{
-			if (builtin.find(tokens[1]) != builtin.end())
-				std::cout << tokens[1] << " is a shell builtin";
+			std::string file = tokens.at(1);
+			const char *path = std::getenv("PATH");
+			std::vector<std::string> dirs = split(path, separator); // make it os-agnostic
+
+			if (builtin.find(tokens.at(1)) != builtin.end())
+				std::cout << tokens.at(1) << " is a shell builtin";
 			else
-				std::cout << tokens[1] << ": not found";
+			{
+				for (auto &dir : dirs)
+				{
+					std::string pathToFile = dir + tokens.at(1);
+					if (std::filesystem::exists(pathToFile))
+					{
+						if (is_executable(pathToFile))
+							std::cout << file << " is " << pathToFile;
+						else
+							continue;
+					}
+					else
+						std::cout << file << ": not found";
+				}
+			}
 			std::cout << std::endl;
 		}
 
