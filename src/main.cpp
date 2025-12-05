c3c21d3155799c7b0f24a7c6b2eebe6b2182d01a
bshmanair <bhishu888@gmail.com>
2025-12-06 01:25:21 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,15 @@
 
 int main()
 {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+	// Flush after every std::cout / std:cerr
+	std::cout << std::unitbuf;
+	std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+	// TODO: Uncomment the code below to pass the first stage
+	std::cout << "$ ";
+
+	std::string command;
+	std::getline(std::cin, command);
+
+	std::cout << command << ": command not found" << std::endl;
 }
