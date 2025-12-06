e1b17a8042ea83aa007141af3b2bfd93ee25b48b
bshmanair <bhishu888@gmail.com>
2025-12-06 22:58:11 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,11 @@
 #include <filesystem>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <optional>
 
 std::vector<std::string> split(const std::string &str, const char delimiter);
 bool is_executable(const std::filesystem::path &p);
-std::string searchExecutable(const std::string &filename);
+std::optional<std::filesystem::path> searchExecutable(const std::string &filename, std::filesystem::path &path);
 
 #if _WIN32
 char separator = ';';
@@ -17,7 +18,7 @@ char separator = ';';
 char separator = ':';
 #endif
 
-std::unordered_set<std::string> builtin = {"exit", "echo", "type"};
+std::unordered_set<std::string> builtin = {"exit", "echo", "type", "pwd"};
 const char *path = std::getenv("PATH");
 std::vector<std::string> dirs = split(path, separator);
 
@@ -26,101 +27,65 @@ int main()
 	// Flush after every std::cout / std:cerr
 	std::cout << std::unitbuf;
 	std::cerr << std::unitbuf;
-
 	std::string input;
-
 	do
 	{
 		std::cout << "$ ";
 		std::getline(std::cin, input);
 		std::vector<std::string> tokens = split(input, ' ');
 		std::string command = tokens.at(0);
-
 		if (command == "exit")
-		{
 			return 0;
-		}
 		else if (command == "echo")
 		{
 			for (int i = 1; i < tokens.size(); i++)
-			{
 				std::cout << tokens.at(i) << " ";
-			}
 			std::cout << std::endl;
 		}
 		else if (command == "type")
 		{
 			std::string file = tokens.at(1);
+			// See if it's a builtin
 			if (builtin.find(file) != builtin.end())
 			{
 				std::cout << tokens.at(1) << " is a shell builtin" << std::endl;
 				continue;
 			}
-			bool found = false;
-			for (auto &dir : dirs)
-			{
-				if (dir.empty())
-				{
-					continue;
-				}
-				std::filesystem::path full = std::filesystem::path(dir) / file;
-				if (std::filesystem::exists(full) && is_executable(full))
-				{
-					std::cout << file << " is " << full.string() << std::endl;
-					found = true;
-					break;
-				}
-			}
-			if (!found)
+
+			// Check if it's an executable
+			auto path = searchExecutable(file);
+			if (path)
+				std::cout << file << " is " << path->string() << std::endl;
+			else
 				std::cout << file << ": not found" << std::endl;
+		}
+		else if (command == "pwd") // TODO: argument length check
+		{
+			std::cout << std::filesystem::current_path().string() << std::endl;
 		}
 		else
 		{
-			// custom_exe_1234 alice
-			// search for exec with given name
-			// if found, execute
-			// pass arguments to program too
-			// make sure to fork it
-
-			std::vector<char *> args; // extract arguments from tokens
+			std::vector<char *> args;
 			for (int i = 0; i < tokens.size(); i++)
-			{
 				args.push_back(const_cast<char *>(tokens.at(i).c_str()));
-			}
 			args.push_back(nullptr);
 
-			bool found = false; // existence check
-			for (auto &dir : dirs)
-			{
-				if (dir.empty())
-				{
-					continue;
-				}
-				std::filesystem::path full = std::filesystem::path(dir) / command;
-				if (std::filesystem::exists(full) && is_executable(full))
-				{
-					found = true;
-					break;
-				}
-			}
-			if (!found)
+			if (searchExecutable(command))
 			{
 				std::cout << command << ": not found" << std::endl;
+				continue;
+			}
+			pid_t processID = fork();
+			if (processID == 0)
+			{
+				execvp(args[0], args.data());
+				std::cerr << "execvp failed" << std::endl;
+				std::exit(1);
 			}
 			else
 			{
-				pid_t processID = fork();
-				if (processID == 0)
-				{
-					execvp(args[0], args.data());
-					std::cerr << "execvp failed" << std::endl;
-					std::exit(1);
-				}
-				else
-				{
-					int status;
-					waitpid(processID, &status, 0);
-				}
+				int status;
+				waitpid(processID, &status, 0);
 			}
 		}
 	} while (true);
@@ -143,4 +108,17 @@ bool is_executable(const std::filesystem::path &p)
 	using namespace std::filesystem;
 	auto pr = status(p).permissions();
 	return (pr & (perms::owner_exec | perms::group_exec | perms::others_exec)) != perms::none;
+}
+
+std::optional<std::filesystem::path> searchExecutable(const std::string &filename)
+{
+	for (const auto &dir : dirs)
+	{
+		if (dir.empty())
+			continue;
+		std::filesystem::path full = std::filesystem::path(dir) / filename;
+		if (std::filesystem::exists(full) && is_executable(full))
+			return full;
+	}
+	return std::nullopt;
 }
